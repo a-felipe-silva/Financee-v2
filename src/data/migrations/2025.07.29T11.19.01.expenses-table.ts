2e3781b888cf398337a40d7c318e3499a51509ea
Agostinho Felipe <agostinhofelipeneto@outlook.com>
2025-07-29 14:49:59 -0300

    Add expense and expense_category tables

---
+++ src/data/migrations/2025.07.29T11.19.01.expenses-table.ts
@@ -0,0 +1,83 @@
+import { Sequelize } from "sequelize";
+import { Migration } from "../../umzug";
+import { DataType } from "sequelize-typescript";
+
+export const up: Migration = async (params: { context: Sequelize }) => {
+  await params.context.getQueryInterface().createTable("expense_category", {
+    id: {
+      type: DataType.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      autoIncrementIdentity: true,
+    },
+    userId: {
+      type: DataType.UUID,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+    name: DataType.STRING(100),
+    createdAt: {
+      type: DataType.DATE,
+      defaultValue: DataType.NOW,
+    },
+    updatedAt: {
+      type: DataType.DATE,
+      defaultValue: DataType.NOW,
+    },
+  });
+
+  await params.context.getQueryInterface().createTable(
+    "expense",
+    {
+      id: {
+        type: DataType.UUID,
+        defaultValue: DataType.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      userId: {
+        type: DataType.UUID,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      categoryId: {
+        type: DataType.INTEGER,
+        references: {
+          model: "expense_category",
+          key: "id",
+        },
+      },
+      description: DataType.STRING(300),
+      amount: DataType.DECIMAL(10, 2),
+      date: {
+        type: DataType.DATE,
+        defaultValue: DataType.NOW,
+      },
+      createdAt: {
+        type: DataType.DATE,
+        defaultValue: DataType.NOW,
+      },
+      updatedAt: {
+        type: DataType.DATE,
+        defaultValue: DataType.NOW,
+      },
+    },
+    {
+      logging: console.log,
+    }
+  );
+};
+
+export const down: Migration = async (params: { context: Sequelize }) => {
+  await params.context.getQueryInterface().dropTable("expense", {
+    logging: console.log,
+  });
+
+  await params.context.getQueryInterface().dropTable("expense_category", {
+    logging: console.log,
+  });
+};
